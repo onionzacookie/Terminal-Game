ddfd480f62089c9c71841867fd12fc42ed292ed2
Anshu Zutshi <anshuzutshi@Anshus-MacBook-Pro.local>
2026-04-15 18:19:54 -0500

    Added AI to play against

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,12 +1,11 @@
-
-#introduce game with empty board
-#set up a list for the board positions, if person types 1, put that in 
+import random
 
 print("Welcome to the Tic-Tac-Toe game!")
 print("Type the corresponding number to your desired position when it is your turn.")
 
 template_board = {i+1: i+1 for i in range(9)}
 game_board = {i+1: " " for i in range(9)}
+available_moves = [i+1 for i in range(9)]
 
 def print_board(board):
     print()
@@ -45,46 +44,66 @@ while True:
     else:
         print("That was not the ENTER key.")
 
+while True:
+    game_mode = input("Press 1 if you want to play with another person or 2 if you want to play against an A1. ")
+    if game_mode in ["1", "2"]:
+        break
+    else:
+        print("That was not a 1 or 2. Retry.")
+
 def play_game():      
-    for i in range(9):
-        if i % 2 == 0:
-            while True:
-                move_string = input("Player 1's turn: Enter a position. ")
-                if move_string in "123456789" and game_board[int(move_string)] == " ":
-                    game_board[int(move_string)] = "X"
-                    print_board(game_board)
-                    if check_win() == True:
-                        print("Player 1 won!")
-                        return
-                    break
-                else:
-                    print("That position is already taken. Please enter an open one. ")
-        else:
-            while True:
-                move_string = input("Player 2's turn: Enter a position. ")
-                if move_string in "123456789" and game_board[int(move_string)] == " ":
-                    game_board[int(move_string)] = "O"
-                    print_board(game_board)
-                    if check_win() == True:
-                        print("Player 2 won!")
-                        return
-                    break
-                else:
-                    print("That position is already taken. Please enter an open one. ")
-    print("It's a tie!")
+    if int(game_mode) == 1:
+        for i in range(9):
+            if i % 2 == 0:
+                while True:
+                    move_string = input("Player 1's turn: Enter a position. ")
+                    if move_string in "123456789" and game_board[int(move_string)] == " ":
+                        game_board[int(move_string)] = "X"
+                        print_board(game_board)
+                        if check_win() == True:
+                            print("Player 1 won!")
+                            return
+                        break
+                    else:
+                        print("That position is already taken. Please enter an open one. ")
+            else:
+                while True:
+                    move_string = input("Player 2's turn: Enter a position. ")
+                    if move_string in "123456789" and game_board[int(move_string)] == " ":
+                        game_board[int(move_string)] = "O"
+                        print_board(game_board)
+                        if check_win() == True:
+                            print("Player 2 won!")
+                            return
+                        break
+                    else:
+                        print("That position is already taken. Please enter an open one. ")
+        print("It's a tie!")
+    elif int(game_mode) == 2:
+        for i in range(9):
+            if i % 2 == 0:
+                while True:
+                    move_string = input("Your turn: Enter a position. ")
+                    if move_string in "123456789" and game_board[int(move_string)] == " ":
+                        available_moves.remove(int(move_string))
+                        game_board[int(move_string)] = "X"
+                        print_board(game_board)
+                        if check_win() == True:
+                            print("You won!")
+                            return
+                        break
+                    else:
+                        print("That position is already taken. Please enter an open one. ")
+            else:
+                move_integer = random.choice(available_moves)
+                available_moves.remove(move_integer)
+                game_board[move_integer] = "O"
+                print_board(game_board)
+                if check_win() == True:
+                    print("The AI won!")
+                    return
+                    
+        print("It's a tie!")
 
 play_game()
 print("Thanks for playing!")
-
-    
-            
-    
-
-
-
-
-
-
-
-
-#print("  |  |  \n--------\n  |  |   \n--------\n  |  |   ")
